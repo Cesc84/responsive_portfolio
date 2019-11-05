201e21fd7d3e488276efb92cbb3dde81593c830e
francesco <francesco@francescos-iMac.local>
2019-11-05 17:25:08 +0000

    h1 after element position

---
+++ dist/thanks.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/main.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"> </script>
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.11"></script>
+  <title>Portfolio - FD</title>
+</head>
+
+<body>
+  <div class="container">
+
+    <!-- HEADER -->
+    <header>
+      <!-- 3 lines - menu icon -->
+      <div class="menu-btn">
+        <div class="btn-line"></div>
+        <div class="btn-line"></div>
+        <div class="btn-line"></div>
+      </div>
+      <nav class="menu">
+
+        <!-- Mobile Menu -->
+        <ul class="menu-nav">
+          <li class="nav-item item"><a href="#" class="nav-link"><span class="highlight">Logo</span></a></li>
+          <li class="nav-item item work"><a href="#" class="nav-link">Work</a></li>
+          <li class="nav-item item about"><a href="#" class="nav-link">About</a></li>
+          <li class="nav-item item contact"><a href="#" class="nav-link">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- MAIN -->
+    <main id="main">
+
+      <!-- Hero Section -->
+      <section id="hero">
+        <div class="row">
+          <div class="headings">
+            <h2>Thanks for contacting me!</h2>
+            <h4>I will reply to you as soon as possible.</h4>
+          </div>
+          <ul class="icons">
+            <li class="item"><a href="https://www.facebook.com/stile.stilo.7" title="Facebook">Fb</a></li>
+            <li class="item"><a href="https://www.instagram.com/francesco_daloe/" title="Instagram">In</a></li>
+            <li class="item"><a href="https://www.linkedin.com/in/francesco-d-alo%C3%A8-268236101/" title="Linkedin">Li</a></li>
+            <li class="item"><a href="https://github.com/Cesc84" title="Github">Git</a></li>
+          </ul>
+        </div>
+      </section>
+
+    </main>
+  </div>
+
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
